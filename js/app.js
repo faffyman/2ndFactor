bc3a7f7c3d8fe94deb6b5d223ff6a8165b94c926
faffyman <faffyman@gmail.com>
2013-06-05 00:27:36 +0100

    Added Settings view for adding new codes

--- js/app.js
+++ js/app.js
@@ -163,7 +163,17 @@ var KeysController = function() {
             secret = secret.replace(/ /g, '');
             if(secret != '') {
                 addAccount(name, secret);
+
+                var viewSettings = document.querySelector("#settings-view");
+                    viewSettings.classList.remove('move-up');
+                    viewSettings.classList.add('move-down');
+            } else {
+                alert('No Secret specified');
+                return false;
             }
+
+
+
         });
     }
 
@@ -193,9 +203,13 @@ var KeysController = function() {
         ctx.shadowOffsetY = 0;
         ctx.shadowColor = '#555555';
 
+        //ctx.moveTo(11,11);
         ctx.arc(11,11,10, deg(0), deg(6 * (sec*2) ) );
+        //ctx.moveTo(11,11);
+        ctx.fillStyle = '#FF4E00';
         ctx.lineWidth = 2;
         ctx.stroke();
+        //ctx.fill();
 
         ctx.font = '1rem "Open Sans", Verdana, Helvetica';
         ctx.fillStyle = '#FF4E00';
@@ -288,17 +302,22 @@ var KeysController = function() {
 $(function() {
 
     // Write your app here
-
     var keysController = new KeysController();
     keysController.init();
 
-    // Bind buttons
-    $('#addbtn, #backbtn, #add').click(function(){
-        //toggle visibility of new-account-entry
-        $('#new-account-entry').toggleClass('focus');
-        return false;
-    });
 
+    //Show / Hide Settings view
+    var btnSettings = document.querySelector("#settings-btn");
+    var viewSettings = document.querySelector("#settings-view");
+    btnSettings.addEventListener ('click', function () {
+        viewSettings.classList.remove('move-down');
+        viewSettings.classList.add('move-up');
+    });
+    var btnCloseSettings = document.querySelector("#close-btn");
+    btnCloseSettings.addEventListener ('click', function () {
+        viewSettings.classList.remove('move-up');
+        viewSettings.classList.add('move-down');
+    });
 
 
 });
